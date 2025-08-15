0b4ec3ce3aa845c73b580196e8d521a110ec7bb8
durga56789 <itsdurgadas@yahoo.co.in>
2025-08-16 00:15:38 +0530

    PrescriptionController.java

--- app/src/main/java/com/project/back_end/models/PrescriptionController.java
+++ app/src/main/java/com/project/back_end/models/PrescriptionController.java
@@ -1,187 +1,51 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package model;
+package controller;
 
-/**
- *
- * @author RIfat
- */
-public class Prescription {
-    private int sl;
-    private int id;
-    private int patientId;
-    private String patientName;
-    private String date;
-    private String cc;
-    private String oe;
-    private String pd;
-    private String dd;
-    private String labWorkUp;
-    private String advice;
-    private String nextVisit;
+import model.Prescription;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-    public Prescription() {
-        
+@RestController
+@RequestMapping("/api/prescriptions")
+public class PrescriptionController {
+
+    // In a real app, you would inject a PrescriptionService here
+    // @Autowired
+    // private PrescriptionService prescriptionService;
+
+    @PostMapping
+    public ResponseEntity<?> savePrescription(@RequestBody Prescription prescription) {
+        if (prescription == null || prescription.getPatientId() <= 0) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(new ApiResponse(false, "Invalid prescription data"));
+        }
+
+        // Here we would call a service to save to DB, e.g.:
+        // Prescription saved = prescriptionService.save(prescription);
+
+        // For now, just simulate a successful save
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ApiResponse(true, "Prescription saved successfully"));
     }
 
-    public Prescription(int id, String date) {
-        this.id = id;
-        this.date = date;
-    }
-    
-    
+    // Simple inner class for consistent API responses
+    static class ApiResponse {
+        private boolean success;
+        private String message;
 
-    public Prescription(int id, int patientId, String date, String cc, String oe, String pd, String dd, String labWorkUp, String advice, String nextVisit) {
-        this.id = id;
-        this.patientId = patientId;
-        this.date = date;
-        this.cc = cc;
-        this.oe = oe;
-        this.pd = pd;
-        this.dd = dd;
-        this.labWorkUp = labWorkUp;
-        this.advice = advice;
-        this.nextVisit = nextVisit;
-    }
-    
-    
+        public ApiResponse(boolean success, String message) {
+            this.success = success;
+            this.message = message;
+        }
 
-    public Prescription(int id, int patientId, String patientName, String date, String cc, String oe, String pd, String dd, String labWorkUp, String advice, String nextVisit) {
-        this.id = id;
-        this.patientId = patientId;
-        this.patientName = patientName;
-        this.date = date;
-        this.cc = cc;
-        this.oe = oe;
-        this.pd = pd;
-        this.dd = dd;
-        this.labWorkUp = labWorkUp;
-        this.advice = advice;
-        this.nextVisit = nextVisit;
-    }
+        public boolean isSuccess() {
+            return success;
+        }
 
-    public Prescription(int sl, int id, int patientId, String patientName, String date, String cc, String oe, String pd, String dd, String labWorkUp, String advice, String nextVisit) {
-        this.sl = sl;
-        this.id = id;
-        this.patientId = patientId;
-        this.patientName = patientName;
-        this.date = date;
-        this.cc = cc;
-        this.oe = oe;
-        this.pd = pd;
-        this.dd = dd;
-        this.labWorkUp = labWorkUp;
-        this.advice = advice;
-        this.nextVisit = nextVisit;
+        public String getMessage() {
+            return message;
+        }
     }
-    
-    
-
-    public int getSl() {
-        return sl;
-    }
-
-    public void setSl(int sl) {
-        this.sl = sl;
-    }
-    
-    
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getPatientId() {
-        return patientId;
-    }
-
-    public void setPatientId(int patientId) {
-        this.patientId = patientId;
-    }
-
-    public String getPatientName() {
-        return patientName;
-    }
-
-    public void setPatientName(String patientName) {
-        this.patientName = patientName;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public String getCc() {
-        return cc;
-    }
-
-    public void setCc(String cc) {
-        this.cc = cc;
-    }
-
-    public String getOe() {
-        return oe;
-    }
-
-    public void setOe(String oe) {
-        this.oe = oe;
-    }
-
-    public String getPd() {
-        return pd;
-    }
-
-    public void setPd(String pd) {
-        this.pd = pd;
-    }
-
-    public String getDd() {
-        return dd;
-    }
-
-    public void setDd(String dd) {
-        this.dd = dd;
-    }
-
-    public String getLabWorkUp() {
-        return labWorkUp;
-    }
-
-    public void setLabWorkUp(String labWorkUp) {
-        this.labWorkUp = labWorkUp;
-    }
-
-    public String getAdvice() {
-        return advice;
-    }
-
-    public void setAdvice(String advice) {
-        this.advice = advice;
-    }
-
-    public String getNextVisit() {
-        return nextVisit;
-    }
-
-    public void setNextVisit(String nextVisit) {
-        this.nextVisit = nextVisit;
-    }
-
-    
-    
-    
-   
-    
-    
 }
